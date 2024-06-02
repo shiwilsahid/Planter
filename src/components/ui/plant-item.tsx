8b27fc8622437bc890351c4f9423afc54a773393
shiwilsahid <sieengelmein@gmail.com>
2024-06-02 12:02:56 +0700

    feat: render list

--- src/components/ui/plant-item.tsx
+++ src/components/ui/plant-item.tsx
@@ -2,8 +2,15 @@ import { Plant } from "../../data/plants";
 
 export function PlantItem({ plant }: { plant: Plant }) {
   return (
-    <div>
-      <p>{plant.text}</p>
+    <div className="flex justify-between">
+      <p className="text-1xl">{plant.text}</p>
+
+      <div className="flex gap-4">
+        {plant.watering && <p>&#9810;</p>}
+        {plant.fertilization && <p>&#9855;</p>}
+        {plant.harvest && <p>&#9935;</p>}
+        <p className="text-sm dark:text-slate-400">{plant.category}</p>
+      </div>
     </div>
   );
 }
--- src/data/plants.ts
+++ src/data/plants.ts
@@ -2,33 +2,51 @@ export type Plant = {
   id: number;
   text: string;
   category: string;
+  watering: boolean;
+  fertilization: boolean;
+  harvest: boolean;
 };
 
 export const dataPlants: Plant[] = [
   {
     id: 1,
     text: "Apple",
-    category: "Watering",
+    category: "Fruit",
+    watering: true,
+    fertilization: false,
+    harvest: false,
   },
 
   {
     id: 2,
-    text: "Banana",
-    category: "Fertilization",
+    text: "Bayam",
+    category: "Spinach",
+    watering: true,
+    fertilization: true,
+    harvest: false,
   },
   {
     id: 3,
-    text: "Cerry",
-    category: "Fertilization",
+    text: "Cabbage",
+    category: "Spinach",
+    watering: true,
+    fertilization: true,
+    harvest: false,
   },
   {
     id: 4,
     text: "Melon",
-    category: "Harvest",
+    category: "Fruit",
+    watering: false,
+    fertilization: true,
+    harvest: false,
   },
   {
     id: 5,
     text: "Watermelon",
-    category: "Harvest",
+    category: "Fruit",
+    watering: false,
+    fertilization: false,
+    harvest: true,
   },
 ];
