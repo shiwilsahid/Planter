e5ef0300ec1594611c253be6a12225c9959c92b0
shiwilsahid <sieengelmein@gmail.com>
2024-06-09 11:56:58 +0700

    feat: change icon to text

--- src/components/ui/plant-item.tsx
+++ src/components/ui/plant-item.tsx
@@ -5,10 +5,10 @@ export function PlantItem({ plant }: { plant: Plant }) {
     <div className="flex justify-between">
       <p className="text-1xl">{plant.text}</p>
 
-      <div className="flex gap-4">
-        {plant.watering && <p>&#9810;</p>}
-        {plant.fertilization && <p>&#9855;</p>}
-        {plant.harvest && <p>&#9935;</p>}
+      <div className="flex gap-4 text-sm">
+        {plant.watering && <p>Watering</p>}
+        {plant.fertilization && <p>fertilization</p>}
+        {plant.harvest && <p>harvest</p>}
         <p className="text-sm dark:text-slate-400">{plant.category}</p>
       </div>
     </div>
